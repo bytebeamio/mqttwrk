db859abe8ebba86cbb94355184d831ed517ce7bf
Amit Kumar <amrx101@gmail.com>
2020-08-14 13:14:33 +0530

    Added delay and removed unwrap

--- src/connection.rs
+++ src/connection.rs
@@ -155,13 +155,9 @@ async fn requests(topic: String, payload_size: usize, count: usize, requests_tx:
         let publish = PublishRaw::new(&topic, qos, payload).unwrap();
         let publish = Request::PublishRaw(publish);
         interval.tick().await;
-        match requests_tx.send(publish).await {
-            Ok(v) => {},
-            Err(e) => {
-                error!("Error in sending={:?}", e);
-                break;
-            }
-        };
+        if let Err(_) = requests_tx.send(publish).await {
+            break;
+        }
     }
 }
 
