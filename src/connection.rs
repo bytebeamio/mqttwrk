85047e636bb5c12440ca07b49d4a015e4c8ac82a
Amit Kumar <amrx101@gmail.com>
2020-11-01 11:49:34 +0530

    Catch up to upstream.

--- src/connection.rs
+++ src/connection.rs
@@ -1,22 +1,23 @@
+use std::sync::Arc;
 use std::time::Instant;
 use std::{fs, io};
-use std::sync::Arc;
-use async_channel::Sender;
 
 use crate::Config;
 
-use tokio::{task, time, select};
+use rumqttc::*;
+use thiserror::Error;
 use tokio::sync::Barrier;
 use tokio::time::Duration;
-use rumqttc::{MqttOptions, EventLoop, Request, QoS, Incoming, Subscribe, PublishRaw};
-use thiserror::Error;
+use tokio::{pin, select, task, time};
 
 const ID_PREFIX: &str = "rumqtt";
 
 pub(crate) struct Connection {
     id: String,
     config: Arc<Config>,
-    eventloop: EventLoop
+    client: AsyncClient,
+    eventloop: EventLoop,
+    sink: Option<String>,
 }
 
 #[derive(Error, Debug)]
@@ -26,16 +27,25 @@ pub enum ConnectionError {
     #[error("Connection error = {0:?}")]
     Connection(#[from] rumqttc::ConnectionError),
     #[error("Wrong packet = {0:?}")]
-    WrongPacket(Incoming)
+    WrongPacket(Incoming),
 }
 
 impl Connection {
-    pub async fn new(id: usize, config: Arc<Config>) -> Result<Connection, ConnectionError> {
-        let id = format!("{}-{}", ID_PREFIX, id);
+    pub async fn new(
+        id: usize,
+        sink: Option<String>,
+        config: Arc<Config>,
+    ) -> Result<Connection, ConnectionError> {
+        let id = if sink.is_none() {
+            format!("{}-{:05}", ID_PREFIX, id)
+        } else {
+            format!("{}-sink-{}", ID_PREFIX, id)
+        };
+
         let mut mqttoptions = MqttOptions::new(&id, &config.server, config.port);
         mqttoptions.set_keep_alive(config.keep_alive);
         mqttoptions.set_inflight(config.max_inflight);
-        mqttoptions.set_conn_timeout(config.conn_timeout);
+        mqttoptions.set_connection_timeout(config.conn_timeout);
         mqttoptions.set_max_request_batch(10);
 
         if let Some(ca_file) = &config.ca_file {
@@ -49,67 +59,83 @@ impl Connection {
             mqttoptions.set_client_auth(cert, key);
         }
 
-
-        let mut eventloop = EventLoop::new(mqttoptions, 10).await;
-        let requests_tx = eventloop.handle();
+        let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
 
         let sconfig = config.clone();
+        let ssink = sink.clone();
+        let mut subscriber_count = config.subscribers;
+
+        if sink.is_some() {
+            // subscriber count options are invalidated for sink connections
+            subscriber_count = 1;
+        }
+
+        let mut sclient = client.clone();
         task::spawn(async move {
             let qos = get_qos(sconfig.qos);
 
-            // subscribes
-            for i in 0..sconfig.subscribers {
-                // Subscribe to one topic per connection
-                let topic = format!("hello/{}-{}/0/world", ID_PREFIX, i);
-                subscribe(topic, requests_tx.clone(), qos).await;
+            // Sink connections contains 1 subscription connections
+            match ssink {
+                Some(topic) => {
+                    sclient.subscribe(topic, qos).await.unwrap();
+                }
+                None => {
+                    // Subscribe to one topic per connection
+                    for i in 0..sconfig.subscribers {
+                        let topic = format!("hello/{}-{:05}/0/world", ID_PREFIX, i);
+                        sclient.subscribe(topic, qos).await.unwrap();
+                    }
+                }
             }
         });
 
         // Handle connection and subscriptions first
         let mut sub_ack_count = 0;
         loop {
-            let (incoming, _outgoing) = eventloop.poll().await?;
-            if let Some(v) = incoming {
+            let event = eventloop.poll().await?;
+            if let Event::Incoming(v) = event {
                 match v {
-                    Incoming::SubAck(_suback) => sub_ack_count += 1,
-                    Incoming::Connected => (),
-                    incoming => return Err(ConnectionError::WrongPacket(incoming))
+                    Incoming::SubAck(_) => sub_ack_count += 1,
+                    Incoming::ConnAck(_) => (),
+                    incoming => return Err(ConnectionError::WrongPacket(incoming)),
                 }
             }
 
-
-            if sub_ack_count >= config.subscribers {
-                break
+            if sub_ack_count >= subscriber_count {
+                break;
             }
         }
 
         Ok(Connection {
             id,
             config,
-            eventloop
+            client,
+            eventloop,
+            sink,
         })
     }
 
     pub async fn start(&mut self, barrier: Arc<Barrier>) {
+        let start = Instant::now();
         // Wait for all the subscription from other connections to finish
         // while doing ping requests so that broker doesn't disconnect
+        let barrier = barrier.wait();
+        pin!(barrier);
+        // println!("await barrier = {:?}", self.id);
         loop {
             select! {
                 _ = self.eventloop.poll() => {},
-                _ = barrier.wait() => break,
+                _ = &mut barrier => break,
             }
-        } 
-
-        // Wait some time for all subscriptions in rumqttd to be successfull.
-        // This is a workaround because of asynchronous publish and subscribe
-        // paths in rumqttd. 
-        // TODO: Make them sequential in broker and remove this
-        time::delay_for(Duration::from_secs(1)).await;
-
-        if self.id == "rumqtt-0" {
-            println!("All connections and subscriptions ok");
         }
 
+        // println!("done barrier = {:?}", self.id);
+        if self.id == "rumqtt-00000" {
+            println!(
+                "All connections and subscriptions ok. Elapsed = {:?}",
+                start.elapsed().as_secs()
+            );
+        }
 
         let qos = get_qos(self.config.qos);
         let payload_size = self.config.payload_size;
@@ -118,33 +144,42 @@ impl Connection {
         let delay = self.config.delay;
         let id = self.id.clone();
 
-        let requests_tx = self.eventloop.requests_tx.clone();
-        for i in 0..publishers {
-            let topic = format!("hello/{}/{}/world", id, i);
-            let tx = requests_tx.clone();
-            task::spawn(async move {
-                requests(topic, payload_size, count, tx, qos, delay).await;
-            });
-        }
-
         let start = Instant::now();
         let mut acks_count = 0;
         let mut incoming_count = 0;
-        let acks_expected = self.config.count * self.config.publishers;
-        let incoming_expected = self.config.count * self.config.publishers * self.config.subscribers;
+        let mut acks_expected = self.config.count * self.config.publishers;
+        let mut incoming_expected =
+            self.config.count * self.config.publishers * self.config.subscribers;
         let mut outgoing_elapsed = Duration::from_secs(0);
         let mut incoming_elapsed = Duration::from_secs(0);
         let mut outgoing_done = false;
         let mut incoming_done = false;
 
+        // Sink connections are single subscription connections
+        if self.sink.is_none() {
+            for i in 0..publishers {
+                let topic = format!("hello/{}/{}/world", self.id, i);
+                let client = self.client.clone();
+                task::spawn(async move {
+                    requests(topic, payload_size, count, client, qos, delay).await;
+                });
+            }
+        } else {
+            acks_expected = 0;
+            incoming_expected =
+                self.config.connections * self.config.count * self.config.publishers;
+        }
+
         let mut reconnects: i32 = 0;
         loop {
-            let (incoming, _outgoing) = match self.eventloop.poll().await {
+            let event = match self.eventloop.poll().await {
                 Ok(v) => v,
                 Err(e) => {
                     error!("Id = {}, Connection error = {:?}", self.id, e);
                     reconnects += 1;
-                    if reconnects == 1 { break }
+                    if reconnects == 1 {
+                        break;
+                    }
 
                     continue;
                 }
@@ -152,19 +187,21 @@ impl Connection {
 
             // Never exit during idle connection tests
             if self.config.publishers == 0 || self.config.count == 0 {
-                continue
+                continue;
             }
 
-            if let Some(v) = incoming {
+            // println!("Id = {}, {:?}", id, incoming);
+
+            if let Event::Incoming(v) = event {
                 match v {
-                   Incoming::PubAck(_pkid) => acks_count += 1,
-                   Incoming::Publish(_publish) => incoming_count += 1,
-                   Incoming::PingResp => continue,
-                   incoming => {
-                       error!("Unexpected incoming packet = {:?}", incoming);
-                       break;
-                   }
-               }
+                    Incoming::PubAck(_pkid) => acks_count += 1,
+                    Incoming::Publish(_publish) => incoming_count += 1,
+                    Incoming::PingResp => {}
+                    incoming => {
+                        error!("Id = {}, Unexpected incoming packet = {:?}", id, incoming);
+                        break;
+                    }
+                }
             }
 
             if !outgoing_done && acks_count >= acks_expected {
@@ -172,18 +209,19 @@ impl Connection {
                 outgoing_done = true;
             }
 
-            if !incoming_done && incoming_count >= incoming_expected  {
+            if !incoming_done && incoming_count >= incoming_expected {
                 incoming_elapsed = start.elapsed();
                 incoming_done = true;
             }
 
             if outgoing_done && incoming_done {
-                break
+                break;
             }
         }
 
         let outgoing_throughput = (acks_count * 1000) as f32 / outgoing_elapsed.as_millis() as f32;
-        let incoming_throughput = (incoming_count * 1000) as f32 / incoming_elapsed.as_millis() as f32;
+        let incoming_throughput =
+            (incoming_count * 1000) as f32 / incoming_elapsed.as_millis() as f32;
 
         println!(
             "Id = {}
@@ -200,35 +238,32 @@ impl Connection {
     }
 }
 
-
 /// make count number of requests at specified QoS.
-async fn requests(topic: String, payload_size: usize, count: usize, requests_tx: Sender<Request>, qos: QoS, delay: u64) {
+async fn requests(
+    topic: String,
+    payload_size: usize,
+    count: usize,
+    mut client: AsyncClient,
+    qos: QoS,
+    delay: u64,
+) {
     let mut interval = match delay {
         0 => None,
-        delay => Some(time::interval(time::Duration::from_secs(delay)))
+        delay => Some(time::interval(time::Duration::from_secs(delay))),
     };
 
     for _i in 0..count {
         let payload = vec![0; payload_size];
-        // payload[0] = (i % 255) as u8;
-        let publish = PublishRaw::new(&topic, qos, payload).unwrap();
-        let publish = Request::PublishRaw(publish);
         if let Some(interval) = &mut interval {
             interval.tick().await;
         }
 
         // These errors are usually due to eventloop task being dead. We can ignore the
         // error here as the failed eventloop task would have already printed an error
-        if let Err(_e) = requests_tx.send(publish).await {
-            break
+        if let Err(_e) = client.publish(topic.clone(), qos, false, payload).await {
+            break;
         }
     }
-}
-
-/// create subscriptions for a topic.
-async fn subscribe(topic: String, requests_tx: Sender<Request>, qos: QoS) {
-    let subscription = Subscribe::new(&topic, qos);
-    requests_tx.send(Request::Subscribe(subscription)).await.unwrap();
 }
 
 /// get QoS level. Default is AtLeastOnce.
@@ -237,7 +272,6 @@ fn get_qos(qos: i16) -> QoS {
         0 => QoS::AtMostOnce,
         1 => QoS::AtLeastOnce,
         2 => QoS::ExactlyOnce,
-        _ => QoS::AtLeastOnce
+        _ => QoS::AtLeastOnce,
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ extern crate log;
 
 use std::sync::Arc;
 
-use futures;
-use tokio::task;
-use tokio::sync::Barrier;
 use argh::FromArgs;
+use futures;
 use futures::stream::StreamExt;
+use tokio::sync::Barrier;
+use tokio::task;
 
 mod connection;
 
@@ -76,19 +76,22 @@ struct Config {
     #[argh(option, short = 'q', default = "1")]
     qos: i16,
 
-    /// number of publishers, default 1
+    /// number of publishers per connection, default 1
     #[argh(option, short = 'x', default = "1")]
     publishers: usize,
 
-    /// number of subscribers, default 1
-    #[argh(option, short = 'y', default = "1")]
+    /// number of subscribers per connection, default 1
+    #[argh(option, short = 'y', default = "0")]
     subscribers: usize,
+
+    /// sink connection 1
+    #[argh(option, short = 's')]
+    sink: Option<String>,
 
     /// delay in between each request in secs
     #[argh(option, short = 'd', default = "0")]
     delay: u64,
 }
-
 
 #[tokio::main(core_threads = 4)]
 async fn main() {
@@ -96,35 +99,58 @@ async fn main() {
 
     let config: Config = argh::from_env();
     let config = Arc::new(config);
-    let barrier = Arc::new(Barrier::new(config.connections));
+    let connections = if config.sink.is_some() {
+        config.connections + 1
+    } else {
+        config.connections
+    };
+    let barrier = Arc::new(Barrier::new(connections));
     let mut handles = futures::stream::FuturesUnordered::new();
 
-    // We synchronously finish connections and subscriptions and then spawn connection
-    // start to perform publishes concurrently. This simplifies 2 things
-    // * Creating too many connections wouldn't lead to `Elapsed` error because
-    //   broker accepts connections sequentially
-    // * We don't have to synchronize all subscription with a barrier because
-    //   subscriptions shouldn't happen after publish to prevent wrong incoming
-    //   publish count
+    // We synchronously finish connections and subscriptions and then spawn
+    // connection start to perform publishes concurrently.
+    //
+    // This simplifies 2 things
+    // * Spawning too many connections wouldn't lead to `Elapsed` error
+    //   in last spawns due to broker accepting connections sequentially
+    // * We have to synchronize all subscription with a barrier because
+    //   subscriptions shouldn't happen after publish to prevent wrong
+    //   incoming publish count
+    //
+    // But the problem which doing connection synchronously (next connectoin
+    // happens only after current connack is recived) is that remote connections
+    // will take a long time to establish 10K connection (much greater than
+    // 10K * 1 millisecond)
     for i in 0..config.connections {
-        let mut connection = match connection::Connection::new(i, config.clone()).await {
+        let mut connection = match connection::Connection::new(i, None, config.clone()).await {
             Ok(c) => c,
             Err(e) => {
                 error!("Device = {}, Error = {:?}", i, e);
-                return
+                return;
             }
         };
 
+        let barrier = barrier.clone();
+        handles.push(task::spawn(async move { connection.start(barrier).await }));
+    }
+
+    if let Some(filter) = config.sink.as_ref() {
+        let mut connection =
+            match connection::Connection::new(1, Some(filter.to_owned()), config.clone()).await {
+                Ok(c) => c,
+                Err(e) => {
+                    error!("Device = sink-1, Error = {:?}", e);
+                    return;
+                }
+            };
 
         let barrier = barrier.clone();
-        handles.push(task::spawn(async move { 
-            connection.start(barrier).await 
-        }));
+        handles.push(task::spawn(async move { connection.start(barrier).await }));
     }
 
     loop {
         if handles.next().await.is_none() {
-            break
+            break;
         }
     }
 }
